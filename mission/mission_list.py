37b71e8250f093afb0b24868d6098506c876117d
GreenDevth <theerapong@ru.ac.th>
2022-02-09 00:48:07 +0700

    add mission list

---
+++ mission/mission_list.py
@@ -0,0 +1,72 @@
+
+guild_master_img = "https://cdn.discordapp.com/attachments/918401567529529364/933710980934942770/banner1.png"
+weapon = [
+    "https://cdn.discordapp.com/attachments/894251225237848134/918039061061586994/kar98.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040553977937960/weapon_0000_Layer-Comp-1.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040554162515998/weapon_0001_Layer-Comp-2.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918039061061586994/kar98.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040554690990090/weapon_0002_Layer-Comp-3.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040555030716436/weapon_0003_Layer-Comp-4.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040555341115402/weapon_0004_Layer-Comp-5.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040553977937960/weapon_0000_Layer-Comp-1.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918039061061586994/kar98.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040554162515998/weapon_0001_Layer-Comp-2.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040553977937960/weapon_0000_Layer-Comp-1.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040554162515998/weapon_0001_Layer-Comp-2.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040554690990090/weapon_0002_Layer-Comp-3.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040555030716436/weapon_0003_Layer-Comp-4.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040554690990090/weapon_0002_Layer-Comp-3.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040555341115402/weapon_0004_Layer-Comp-5.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040555030716436/weapon_0003_Layer-Comp-4.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040555341115402/weapon_0004_Layer-Comp-5.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/918040555605348382/weapon_0005_Layer-Comp-6.png"
+
+]
+
+
+foods = [
+    "https://cdn.discordapp.com/attachments/894251225237848134/935911568640720976/v13.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935911568451993680/v12.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935911568259043338/v11.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935911568045117450/v10.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935911487740997632/v9.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935911487392854047/v8.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935911487153770556/v7.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935911486931484723/v6.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935911486545621032/v5.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935911486335897630/v4.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935911486084231188/v3.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935911485874520074/v2.png"
+
+]
+
+
+fishing = [
+    "https://cdn.discordapp.com/attachments/894251225237848134/935886032610689072/fishing_03.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935886032241565706/fishing_02.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935886031880851456/fishing_01.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935885932245164102/fishing_10.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935885932035461130/fishing_09.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935885931779604520/fishing_08.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935885931540516884/fishing_07.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935885931326631996/fishing_06.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935885931108507658/fishing_05.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935885931108507658/fishing_05.png"
+]
+
+
+animal = [
+    "https://cdn.discordapp.com/attachments/894251225237848134/935908000265306142/a4.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935907999980085308/a3.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935907999686479932/a2.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935907999413858315/a1.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935908000521125908/a5.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935908000747622400/a6.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935908093462724648/a7.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935908093680832622/a8.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935908093915721729/a9.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935908094125416588/a10.png",
+    "https://cdn.discordapp.com/attachments/894251225237848134/935907999413858315/a1.png"
+]
+
+
