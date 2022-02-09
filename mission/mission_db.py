7ba0bf611fe4a6a627a8757b410622e80f34b049
GreenDevth <theerapong@ru.ac.th>
2022-02-09 22:02:17 +0700

    add Master Guild mission

--- mission/mission_db.py
+++ mission/mission_db.py
@@ -8,7 +8,7 @@ def get_mission(id_mission):
         conn = MySQLConnection(**db)
         cur = conn.cursor()
         cur.execute('SELECT mission_name FROM scum_mission_name WHERE mission_id =%s', (id_mission,))
-        row = cur.fetchall()
+        row = cur.fetchone()
         while row is not None:
             res = list(row)
             return res
