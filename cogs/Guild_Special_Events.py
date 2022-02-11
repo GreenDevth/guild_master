fd17f109c1b857b379118889311c29475b3e9e5c
GreenDevth <theerapong@rumail.ru.ac.th>
2022-02-11 09:32:10 +0700

    Add Special Listener Event

--- cogs/Guild_Special_Events.py
+++ cogs/Guild_Special_Events.py
@@ -2,7 +2,7 @@ import discord
 import random
 from discord.ext import commands
 from discord_components import Button, ButtonStyle
-
+from db.players_db import player_mission, mission_up
 
 class GuildSpecialEventCommand(commands.Cog):
     def __init__(self, bot):
@@ -12,9 +12,25 @@ class GuildSpecialEventCommand(commands.Cog):
     async def on_button_click(self, interaction):
         member = interaction.author
         event_btn = interaction.component.custom_id
-
+        check = player_mission(member.id)
         if event_btn == 'event_1':
+            if check == 0:
+                await interaction.respond(content='Continue get new event mission')
+            await interaction.respond(content='event mission already exists')
+
+        if event_btn == 'report_event_1':
             await interaction.respond(content='ok')
+
+        if event_btn == 'detail_event_1':
+            await interaction.respond(
+                '**นำส่งเซ็ตอาวุธปืน SDASS 12M** ซึ่งประกอบด้วย '
+                '\n- ปืน SDASS 12M 1 กระบอก'
+                '\n- Improvised Flashlight 1 อัน '
+                '\n- OKP-7 Holographic 1 อัน '
+                '\n- Bridshot 1 กล่อง (สีเขียว) '
+                '\n- Buckshot 1 กล่อง (สีแดง) '
+                '\n- Slug 1 กล่อง (สีดำ) ',
+            )
 
     @commands.command(name='special_event')
     async def special_event_command(self, ctx):
