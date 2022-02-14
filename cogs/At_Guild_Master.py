3400d16c67fc6dd5036439fb998998ee14dd3649
GreenDevth <theerapong@rumail.ru.ac.th>
2022-02-14 10:58:17 +0700

    Add special event

---
+++ cogs/At_Guild_Master.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+from discord_components import Button, ButtonStyle
+
+
+class AtGuildMasterCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(name='guild', invoke_without_command=True)
+    @commands.has_role('Admin')
+    async def guild_command(self, ctx):
+        await ctx.reply('This command only used in admin role.')
+        await ctx.message.delete()
+
+    @guild_command.command(name='at_guild')
+    async def at_guild_sub_command(self, ctx):
+        await ctx.send(
+            file=discord.File('./img/mission/mission_center.png'),
+            components=[
+                [
+                    Button(style=ButtonStyle.blue, label='บริการ taxi', emoji='🚘', custom_id='taxi_to_guild'),
+                    Button(style=ButtonStyle.gray, label='ค่าบริการ 100 เหรียญ', emoji='💵', disabled=True)
+                ]
+            ]
+        )
+
+def setup(bot):
+    bot.add_cog(AtGuildMasterCommand(bot))
